7c61ab23ec9514acf9dce977deba992a79fe959b
Gavan Lamb <gavan.jon.lamb@gmail.com>
2022-10-03 21:19:25 +1100

    Remove infra

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,9 +1,6 @@
 ﻿terraform {
   required_version = ">=1.2.5"
-  backend "s3" {
-    key = "terraform.tfstate"
-    encrypt = true
-  }
+  backend "s3" { }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
